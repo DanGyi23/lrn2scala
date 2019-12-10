06eb30686f3f4f096ae80d58e74b624509810828
Dan Gyi <daniel.gyi@gmail.com>
2019-12-10 14:35:04 +0000

    all tests passing

--- fizzbuzz/src/test/scala/example/HelloSpec.scala
+++ fizzbuzz/src/test/scala/example/HelloSpec.scala
@@ -18,4 +18,28 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
   "The FizzBuzz object" should "return FizzBuzz if 15 is passed as query" in {
     FizzBuzz.query(15) shouldEqual "FizzBuzz"
   }
+
+  "The FizzBuzz object" should "return Fizz if 6 is passed as query" in {
+    FizzBuzz.query(6) shouldEqual "Fizz"
+  }
+
+  "The FizzBuzz object" should "return 7 if 7 is passed as query" in {
+    FizzBuzz.query(7) shouldEqual 7
+  }
+
+  "The FizzBuzz object" should "return FizzBuzz if 45 is passed as query" in {
+    FizzBuzz.query(45) shouldEqual "FizzBuzz"
+  }
+
+  "The FizzBuzz object" should "return Fizz if 99 is passed as query" in {
+    FizzBuzz.query(99) shouldEqual "Fizz"
+  }
+
+  "The FizzBuzz object" should "return Buzz if 20 is passed as query" in {
+    FizzBuzz.query(20) shouldEqual "Buzz"
+  }
+
+  "The FizzBuzz object" should "return FizzBuzz if 135 is passed as query" in {
+    FizzBuzz.query(135) shouldEqual "FizzBuzz"
+  }
 }
