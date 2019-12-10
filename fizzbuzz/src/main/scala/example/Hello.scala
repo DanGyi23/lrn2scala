4137d5d15a0624cec2a9adde0a311a022e1ca6c7
Dan Gyi <daniel.gyi@gmail.com>
2019-12-10 14:27:10 +0000

    fizzbuzzing

--- fizzbuzz/src/main/scala/example/Hello.scala
+++ fizzbuzz/src/main/scala/example/Hello.scala
@@ -2,12 +2,12 @@ package example
 
 object FizzBuzz extends App {
   def query(num: Int): Any = {
-    if (num % 3 == 0) {
+    if (num % 15 == 0) {
+      return "FizzBuzz"
+    } else if (num % 3 == 0) {
       return "Fizz"
     } else if (num % 5 == 0) {
       return "Buzz"
-    } else if (num % 15 == 0) {
-      return "FizzBuzz"
     } else {
       return num
     }
--- fizzbuzz/src/test/scala/example/HelloSpec.scala
+++ fizzbuzz/src/test/scala/example/HelloSpec.scala
@@ -14,4 +14,8 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
   "The FizzBuzz object" should "return Buzz if 5 is passed as query" in {
     FizzBuzz.query(5) shouldEqual "Buzz"
   }
+
+  "The FizzBuzz object" should "return FizzBuzz if 15 is passed as query" in {
+    FizzBuzz.query(15) shouldEqual "FizzBuzz"
+  }
 }
